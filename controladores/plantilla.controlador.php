083305287eb76d778e0e104bb469a83ad290732b
ElCiro <ciroivangarciarosas1995@gmail.com>
2023-09-24 23:46:00 -0600

    Mensaje de confirmación

---
+++ controladores/plantilla.controlador.php
@@ -0,0 +1,13 @@
+<?php
+
+Class ControladorPlantilla{
+    /**
+     * llamada a la plantilla
+     */
+    public function ctrTraerPlantilla(){
+        #include()se utiliza para invocar el archivo que contiene
+        #codigo html o php
+        include "vistas/plantilla.php";
+    }
+}
+?>
---
+++ vistas/paginas/inicio.php
@@ -0,0 +1,43 @@
+<?php 
+
+    if(!isset($_SESSION["validarIngreso"])){
+        echo '<script>window.location="index.php?pagina=ingreso";</script>';
+        return;
+    }else{
+        if ($_SESSION["validarIngreso"] != "ok"){
+            echo '<script>window.location="index.php?pagina=ingreso";</script>';
+            return;
+        }
+    }
+
+    $usuarios = ControladorFormularios::ctrSeleccionarRegistros(null,null);
+    //echo '<pre>'; print_r($usuarios); echo '</pre>';
+
+ ?>
+<table class="table table-striped">
+            <thead>
+                <tr>
+                <th>#</th>
+                    <th>Nombre</th>
+                    <th>Email</th>
+                    <th>Fecha</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($usuarios as $key =>$value): ?>
+                <tr>
+                <td><?php echo ($key+1) ?></td>
+                    <td><?php echo $value["nombre"]; ?></td>
+                    <td><?php echo $value["email"]; ?></td>
+                    <td><?php echo $value["f"]; ?></td>
+                    <td>
+                        <div class="btn-group">
+                            <button class="btn btn-warning"><i class="fas fa-pencil-alt"></i></button>
+                            <button class="btn btn-danger"><i class="fas fa-trash-alt"></i></nutton>
+                        </div>
+                    </td>
+                </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
